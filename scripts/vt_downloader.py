7996912723452525fda73a58fce8c4908949a818
Keith J. Jones <keith@keithjjones.com>
2017-04-10 21:40:46 -0400

    Added new functionality for new VT API library additions.

--- scripts/vt_downloader.py
+++ scripts/vt_downloader.py
@@ -1,14 +1,12 @@
 import argparse
 import os
-import json
+from hashlib import sha256
 from virus_total_apis import IntelApi
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Downloads samples from VT Intelligence.')
-    parser.add_argument('InputJSON',
-                        help='The VT Intelligence notifications JSON.')
     parser.add_argument('OutputDirectory',
                         help='The output directory for the samples.')
     parser.add_argument("-a", "--apikey",
@@ -22,14 +20,54 @@ def main():
     except:
         os.mkdir(args.OutputDirectory)
 
-    inputjson = json.loads(open(args.InputJSON).read())
-
     api = IntelApi(args.apikey)
 
-    for notification in inputjson['notifications']:
-        print("Downloading {0}".format(notification['sha256']))
-        api.get_file(notification['sha256'], args.OutputDirectory)
-        print("\tDownloaded {0}".format(notification['sha256']))
+    downloads = 0
+    nextpage = None
+
+    while True:
+        nextpage, results = api.get_intel_notifications_feed(nextpage)
+        results = results.json()
+        print("Downloading {0} Samples..."
+              .format(len(results['notifications'])))
+
+        for notification in results['notifications']:
+            downloads += 1
+            subdir = os.path.join(args.OutputDirectory,
+                                  notification['ruleset_name'])
+            try:
+                os.stat(subdir)
+            except:
+                os.mkdir(subdir)
+
+            try:
+                print(sha256_file(os.path.join(subdir, notification['sha256'])).upper())
+            except:
+                pass
+
+            print("Downloading {0}".format(notification['sha256']))
+
+            if (os.path.isfile(os.path.join(subdir, notification['sha256']))
+                and notification['sha256'].upper() ==
+                    sha256_file(
+                        os.path.join(subdir, notification['sha256'])).upper()):
+                print("\tFile {0} Already Downlaoded!"
+                      .format(notification['sha256']))
+            else:
+                api.get_file(notification['sha256'], subdir)
+                print("\tDownloaded {0}".format(notification['sha256']))
+
+        if nextpage is None:
+            break
+
+    print("Downloaded {0} Total Samples".format(downloads))
+
+
+def sha256_file(filename):
+    hasher = sha256()
+    with open(filename,'rb') as f:
+        hasher.update(f.read())
+    return hasher.hexdigest()
 
 if __name__ == "__main__":
     main()
