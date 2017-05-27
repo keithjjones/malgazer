28d592538b6d7b0f41e46a8c21e52919cad05a9f
Keith J. Jones <keith@keithjjones.com>
2017-05-27 13:32:26 -0400

    Reorganization.

---
+++ batch_statistics.py
@@ -0,0 +1,11 @@
+import argparse
+from library.files import FileObject
+from library.plots import ScatterPlot
+import numpy
+import time
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
