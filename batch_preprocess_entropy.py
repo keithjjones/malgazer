f9ef8faf90bf0cf95815334275c374b6cbf643ce
Keith J. Jones <keith@keithjjones.com>
2018-02-28 11:29:45 -0500

    Input fix up.

--- batch_preprocess_entropy.py
+++ batch_preprocess_entropy.py
@@ -8,23 +8,25 @@ def main():
         description='Calculates the entropy of a file.')
     parser.add_argument('DataDirectory',
                         help='The directory containing malware data to analyze, calculated by batch_calculate_entropy.')
+    parser.add_argument('OutputDirectory',
+                        help='The output directory where the data will be saved.')
     parser.add_argument("-w", "--window",
                         help="Window size, in bytes, for running entropy."
-                             "", type=str, required=False)
+                             "", type=int, default=256, required=False)
     parser.add_argument("-d", "--datapoints",
                         help="The number of data points to sample running window entropy."
-                             "", type=str, required=False)
+                             "", type=int, default=512, required=False)
     args = parser.parse_args()
 
-    extracted_features,df = Utils.batch_tsfresh_rwe_data(args.DataDirectory,
-                                                         args.datapoints,
-                                                         args.window)
+    extracted_features, df = Utils.batch_tsfresh_rwe_data(args.DataDirectory,
+                                                          args.datapoints,
+                                                          args.window)
     classifications = Utils.get_classifications_from_path(args.DataDirectory)
     classifications_ordered = Utils.create_ordered_classifications(classifications,
                                                                    extracted_features)
     Utils.save_processed_data(df, classifications,
                               classifications_ordered,
-                              extracted_features, "data")
+                              extracted_features, args.OutputDirectory)
 
 
 if __name__ == "__main__":
