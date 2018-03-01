55bd85bfafc24825cdd6d63e09efb5ff7109259b
Keith J. Jones <keith@keithjjones.com>
2018-03-01 12:06:46 -0500

    Moved to own file.

---
+++ batch_tsfresh_entropy.py
@@ -0,0 +1,40 @@
+import argparse
+from library.utils import Utils
+
+
+def main(arguments=None):
+    # Argument parsing
+    parser = argparse.ArgumentParser(
+        description='Calculates the entropy of a file.')
+    parser.add_argument('DataDirectory',
+                        help='The directory containing malware data to analyze, calculated by batch_calculate_entropy.')
+    parser.add_argument('OutputDirectory',
+                        help='The output directory where the data will be saved.')
+    parser.add_argument("-w", "--window",
+                        help="Window size, in bytes, for running entropy."
+                             "", type=int, default=256, required=False)
+    parser.add_argument("-d", "--datapoints",
+                        help="The number of data points to sample running window entropy."
+                             "", type=int, default=512, required=False)
+
+    if isinstance(arguments, list):
+        args = parser.parse_args(arguments)
+    else:
+        args = parser.parse_args()
+
+
+    extracted_features, df = Utils.batch_tsfresh_rwe_data(args.DataDirectory,
+                                                          args.datapoints,
+                                                          args.window)
+
+    classifications = Utils.get_classifications_from_path(args.DataDirectory)
+
+    # Utils.extract_tsfresh_relevant_features(extracted_features, classifications_ordered['classification'])
+
+    Utils.save_processed_tsfresh_data(df, classifications,
+                                      extracted_features,
+                                      args.OutputDirectory)
+
+
+if __name__ == "__main__":
+    main()
