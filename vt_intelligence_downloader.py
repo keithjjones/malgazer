bbae3c1b147f88916ee74878b3a2fc98ad6a7f60
Keith J. Jones <keith@keithjjones.com>
2018-03-12 09:25:39 -0400

    Don't download samples already downloaded.

--- vt_intelligence_downloader.py
+++ vt_intelligence_downloader.py
@@ -59,37 +59,38 @@ def main():
                 except:
                     os.makedirs(args.OutputDirectory)
 
-                print("Downloading {0}".format(hash))
-                downloaded = False
-                while downloaded is False:
-                    response = intel_api.get_file(hash, args.OutputDirectory)
-                    print("\tDownloaded {0}".format(hash))
-                    print("\tVerifying hash...")
-                    expected_hash = hash.upper()
-                    dl_hash = sha256_file(filename).upper()
+                if not os.path.isfile(filename):
+                    print("Downloading {0}".format(hash))
+                    downloaded = False
+                    while downloaded is False:
+                        response = intel_api.get_file(hash, args.OutputDirectory)
+                        print("\tDownloaded {0}".format(hash))
+                        print("\tVerifying hash...")
+                        expected_hash = hash.upper()
+                        dl_hash = sha256_file(filename).upper()
 
-                    if expected_hash != dl_hash:
-                        print("**** DOWNLOAD ERROR!  SHA256 Does not match!")
-                        print("\tExpected SHA256: {0}".format(expected_hash))
-                        print("\tCalculated SHA256: {0}".format(dl_hash))
-                        print("\tHave you exceeded your quota?")
-                    else:
-                        print("\t\tHash verified!")
-                        downloads += 1
-                        print("\tDownloaded {0} samples...".format(downloads))
-                        downloaded = True
+                        if expected_hash != dl_hash:
+                            print("**** DOWNLOAD ERROR!  SHA256 Does not match!")
+                            print("\tExpected SHA256: {0}".format(expected_hash))
+                            print("\tCalculated SHA256: {0}".format(dl_hash))
+                            print("\tHave you exceeded your quota?")
+                        else:
+                            print("\t\tHash verified!")
+                            downloads += 1
+                            print("\tDownloaded {0} samples...".format(downloads))
+                            downloaded = True
 
-                file_report = None
-                while file_report is None:
-                    print("\tDownloading file report...")
-                    file_report = public_api.get_file_report(hash)
-                    if 'error' in file_report:
-                        print("\t\t\tError, retrying...")
-                        time.sleep(60)
-                        file_report = None
-                ds = pd.Series(file_report['results'])
-                ds.name = hash.upper()
-                df = df.append(ds)
+                    file_report = None
+                    while file_report is None:
+                        print("\tDownloading file report...")
+                        file_report = public_api.get_file_report(hash)
+                        if 'error' in file_report:
+                            print("\t\t\tError, retrying...")
+                            time.sleep(60)
+                            file_report = None
+                    ds = pd.Series(file_report['results'])
+                    ds.name = hash.upper()
+                    df = df.append(ds)
             else:
                 break
 
