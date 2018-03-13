89fc0032c1a5d00a9bfc469f9191f444eb3bc9a7
Keith J. Jones <keith@keithjjones.com>
2018-03-13 11:40:20 -0400

    Added script to pull hashes from VT data.

---
+++ vt_get_hashes.py
@@ -0,0 +1,19 @@
+import argparse
+import pandas as pd
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Pulls hashes from VT data.')
+    parser.add_argument('VTCSV',
+                        help='The VT CSV file.')
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.VTCSV, index_col=0)
+
+    for index, row in df.iterrows():
+        print(index.lower())
+
+
+if __name__ == "__main__":
+    main()
