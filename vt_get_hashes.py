061482e539dd959f015ecda54a99c0843fe1ac96
Keith J. Jones <keith@keithjjones.com>
2018-03-13 12:17:31 -0400

    Added case option.

--- vt_get_hashes.py
+++ vt_get_hashes.py
@@ -7,12 +7,18 @@ def main():
         description='Pulls hashes from VT data.')
     parser.add_argument('VTCSV',
                         help='The VT CSV file.')
+    parser.add_argument("-l", "--lowercase",
+                        help="Use lower case for hashes."
+                             "", required=True)
     args = parser.parse_args()
 
     df = pd.read_csv(args.VTCSV, index_col=0)
 
     for index, row in df.iterrows():
-        print(index.upper())
+        if args.lowercase:
+            print(index.lower())
+        else:
+            print(index.upper())
 
 
 if __name__ == "__main__":
