9714f6fa689cdc495bda4df7177369a5d8d49d25
Keith J. Jones <keith@keithjjones.com>
2018-04-03 12:53:11 -0400

    Added more VT parsers.

--- library/utils.py
+++ library/utils.py
@@ -713,6 +713,8 @@ class Utils(object):
                     c = c[1]
                 elif c[0].lower() == 'win32':
                     c = c[1]
+                elif c[0].lower() == 'behaveslike':
+                    c = c[2]
                 else:
                     c = c[0]
                 c = c.split(' ')[0]
