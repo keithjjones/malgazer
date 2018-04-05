345e5199e6e2da1fc222731c5821c4ab93a5066c
Keith J. Jones <keith@keithjjones.com>
2018-04-05 10:19:44 -0400

    Refactored.

--- library/utils.py
+++ library/utils.py
@@ -366,31 +366,6 @@ class Utils(object):
         return cls
 
     @staticmethod
-    def parse_classifications_with_delimiter(classifications, column_name, delimiter):
-        """
-        Parses the classification from a DF where the format is:
-        <CLASSIFICATION><DELIMITER><SUBCLASSIFICATION>
-
-        :param classifications:  The DataFrame containing classification info.
-        The DF must have the index as the file hash.
-        :param delimiter: the delimiter for the original classification info in
-        the DataFrame.
-        :param column_name: The column name containing the classification info.
-        :return: A DataFrame of classifications for hashes.
-        """
-        cls = pd.DataFrame(columns=['classification'])
-        for index, row in classifications.iterrows():
-            if isinstance(row[column_name], str):
-                cl = row[column_name]
-                c = cl.split(delimiter)
-                d = dict()
-                d['classification'] = c[0]
-                ds = pd.Series(d)
-                ds.name = index
-                cls = cls.append(ds)
-        return cls
-
-    @staticmethod
     def estimate_vt_classifications_from_csv(filename, products=None):
         """
         Reads a CSV containing VT classifications and attempts to estimate
@@ -815,16 +790,20 @@ class Utils(object):
         return None
 
     @staticmethod
-    def parse_subclassifications_with_delimiter(classifications, column_name, delimiter):
+    def parse_classifications_with_delimiter(classifications,
+                                             column_name,
+                                             delimiter,
+                                             split_index=1):
         """
-        Parses the subclassification from a DF where the format is:
-        <CLASSIFICATION><DELIMITER><SUBCLASSIFICATION>
+        Parses the classification from a DF where the format is:
+        <CLASSIFICATION><DELIMITER><SUBCLASSIFICATION>...
 
         :param classifications:  The DataFrame containing classification info.
         The DF must have the index as the file hash.
         :param delimiter: the delimiter for the original classification info in
         the DataFrame.
         :param column_name: The column name containing the classification info.
+        :param split_index:  The index after the split using the delimiter
         :return: A DataFrame of classifications for hashes.
         """
         cls = pd.DataFrame(columns=['classification'])
@@ -833,7 +812,7 @@ class Utils(object):
                 cl = row[column_name]
                 c = cl.split(delimiter)
                 d = dict()
-                d['classification'] = c[1]
+                d['classification'] = c[split_index]
                 ds = pd.Series(d)
                 ds.name = index
                 cls = cls.append(ds)
