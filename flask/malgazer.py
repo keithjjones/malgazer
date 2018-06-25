9d31db4a1a98d8251c5f145150032ab501f190e2
Keith J. Jones <keith@keithjjones.com>
2018-06-25 14:48:42 -0400

    Added stubs for website.

--- flask/malgazer.py
+++ flask/malgazer.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'Hello, World!'
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
