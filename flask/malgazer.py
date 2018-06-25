b03dcb9271f2633b0101a537d44be096a86bfab0
Keith J. Jones <keith@keithjjones.com>
2018-06-25 12:52:53 -0400

    Got docker working.

--- flask/malgazer.py
+++ flask/malgazer.py
@@ -7,5 +7,5 @@ def hello_world():
     return 'Hello, World!'
 
 
-# if __name__ == '__main__':
-#       app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=8080)
