0df6a3eac7840a8998aaefc1cf059332c45d036d
Keith J. Jones <keith@keithjjones.com>
2018-06-26 11:31:00 -0400

    Refactor.

--- docker/api/malgazer.py
+++ docker/api/malgazer.py
@@ -1,31 +1,12 @@
-import tornado.ioloop
-import tornado.web
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
-        self.write(self.request.body)
-        self.write("This is me!")
-
-    def post(self):
-        for field_name, files in self.request.files.items():
-            print(field_name)
-            print(files)
-            for info in files:
-                filename, content_type = info['filename'], info['content_type']
-                body = info['body']
-                self.write('POST "{}" "{}" "{}" {} bytes\n'.format(field_name, filename, content_type, len(body)))
-        self.write('OK\n')
+@app.route('/')
+def main():
+    return render_template('main.html')
 
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-
-
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=8080)
