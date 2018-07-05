68f78a26a8153e489e0ac592d9dc743140e00824
Keith J. Jones <keith@keithjjones.com>
2018-07-05 10:44:47 -0400

    Refactor.

--- build_hashes.py
+++ build_hashes.py
@@ -5,17 +5,13 @@ import os
 
 pd.set_option('max_colwidth', 64)
 
-feature_type = 'rwe'
 binaries_dir = '/Volumes/JONES/Focused Set May 2018/Binaries'
 datapoints = 1024
 windowsize = 256
-if feature_type == 'rwe':
-    datadir = '/Volumes/JONES/Focused Set May 2018/RWE'
-elif feature_type == 'gist':
-    datadir = '/Volumes/JONES/Focused Set May 2018/GIST'
+datadir = '/Volumes/JONES/Focused Set May 2018/RWE'
 
 # Load data
-all_data, raw_data, classifications = Utils.load_features(datadir, feature_type, windowsize=windowsize, datapoints=datapoints)
+all_data, raw_data, classifications = Utils.load_features(datadir, 'rwe', windowsize=windowsize, datapoints=datapoints)
 
 # Pick 60k samples, 10k from each classification
 trimmed_data = all_data.groupby('classification').head(10000)
