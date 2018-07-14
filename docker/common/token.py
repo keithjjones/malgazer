bd97c896bb36bb11fe3e836089b27a6e3f6d0d4d
Keith J. Jones <keith@keithjjones.com>
2018-07-14 15:08:17 -0400

    Added more to forgotten password mechanism.

--- docker/common/token.py
+++ docker/common/token.py
@@ -12,6 +12,13 @@ def generate_confirmation_token(email):
 
 
 def confirm_token(token, expiration=3600):
+    """
+    This determines if the token is valid.
+
+    :param token:  The token.
+    :param expiration: The expiration, in seconds.
+    :return: False if there is an issue, the email for the token if it is confirmed.
+    """
     serializer = URLSafeTimedSerializer(SECRET_KEY)
     try:
         email = serializer.loads(token, salt=SECURITY_PASSWORD_SALT, max_age=expiration)
