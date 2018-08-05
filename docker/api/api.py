7de89b030b7c09d621ab8f8d98a9b3b91c6ca4b5
Keith J. Jones <keith@keithjjones.com>
2018-08-05 12:29:32 -0400

    Fixed up some standalone issues.

--- docker/api/api.py
+++ docker/api/api.py
@@ -146,44 +146,49 @@ def submit():
     Submits a sample and executes thread to process it.
     """
     ip_addr = get_request_ip()
-    if not MULTIUSER or (MULTIUSER and get_apikey()):
-        api_key = get_apikey()
-        user = User.query.filter_by(api_key=api_key).first()
-        if user:
-            possible_classification = request.form.get('classification', 'Unknown')
-            if 'file' not in request.files:
-                return "ERROR"
-            file = request.files['file']
-            f = file.stream.read()
-            s = Sample(frommemory=f)
-            filename = secure_filename(s.sha256)
-            filepath = os.path.join(SAMPLES_DIRECTORY, filename)
-            if not os.path.isfile(filepath):
-                with open(filepath, 'wb') as f_out:
-                    f_out.write(s.rawdata)
-            submit_time = datetime.datetime.now()
-            submission = Submission(sha256=s.sha256, time=submit_time, ip_address=ip_addr,
-                                    classification='', possible_classification=possible_classification,
-                                    status='Processing')
-            return_data = {'id': submission.id, 'sha256': submission.sha256,
-                           'time': str(submission.time), 'ip_address': submission.ip_address,
-                           'classification': submission.classification,
-                           'possible_classification': submission.possible_classification,
-                           'status': submission.status}
-            db.session.add(submission)
-            db.session.commit()
-            app.logger.info('Submitted sample: {0} from IP: {1}'.format(s.sha256, ip_addr))
-            proc = multiprocessing.Process(target=process_sample, args=(submission.id,))
-            proc.start()
-            # thread = threading.Thread(target=process_sample, args=(submission.id,))
-            # thread.daemon = True
-            # thread.start()
-            # process_sample(submission.id)
-            return json.dumps(return_data), 200
+
+    user_id = None
+    if MULTIUSER:
+        if get_apikey():
+            api_key = get_apikey()
+            user = User.query.filter_by(api_key=api_key).first()
+            if user:
+                user_id = user.id
+            else:
+                return "Bad Request 400: You did not supply a valid API key.", 400
         else:
-            return "Bad Request 400: You did not supply a valid API key.", 400
-    else:
-        return "Bad Request 400: You did not supply an API key.", 400
+            return "Bad Request 400: You did not supply an API key.", 400
+
+    possible_classification = request.form.get('classification', 'Unknown')
+    if 'file' not in request.files:
+        return "ERROR"
+    file = request.files['file']
+    f = file.stream.read()
+    s = Sample(frommemory=f)
+    filename = secure_filename(s.sha256)
+    filepath = os.path.join(SAMPLES_DIRECTORY, filename)
+    if not os.path.isfile(filepath):
+        with open(filepath, 'wb') as f_out:
+            f_out.write(s.rawdata)
+    submit_time = datetime.datetime.now()
+    submission = Submission(sha256=s.sha256, time=submit_time, ip_address=ip_addr,
+                            classification='', possible_classification=possible_classification,
+                            status='Processing', user_id=user_id)
+    return_data = {'id': submission.id, 'sha256': submission.sha256,
+                   'time': str(submission.time), 'ip_address': submission.ip_address,
+                   'classification': submission.classification,
+                   'possible_classification': submission.possible_classification,
+                   'status': submission.status}
+    db.session.add(submission)
+    db.session.commit()
+    app.logger.info('Submitted sample: {0} from IP: {1} from User ID: {2}'.format(s.sha256, ip_addr, user_id))
+    proc = multiprocessing.Process(target=process_sample, args=(submission.id,))
+    proc.start()
+    # thread = threading.Thread(target=process_sample, args=(submission.id,))
+    # thread.daemon = True
+    # thread.start()
+    # process_sample(submission.id)
+    return json.dumps(return_data), 200
 
 
 @app.route("/history")
@@ -193,24 +198,31 @@ def history():
     Lists the history of submissions.
     """
     ip_addr = get_request_ip()
-    if not MULTIUSER or (MULTIUSER and get_apikey()):
-        api_key = get_apikey()
-        user = User.query.filter_by(api_key=api_key).first()
-        if user:
-            submissions = Submission.query.order_by(Submission.id.desc()).all()
-            return_data = []
-            for s in submissions:
-                return_data.append({'id': s.id,
-                                    'sha256': s.sha256, 'time': str(s.time),
-                                    'classification': s.classification,
-                                    'possible_classification': s.possible_classification,
-                                    'ip_address': s.ip_address,
-                                    'status': s.status})
-            return json.dumps(return_data), 200
+
+    user_id = None
+    if MULTIUSER:
+        if get_apikey():
+            api_key = get_apikey()
+            user = User.query.filter_by(api_key=api_key).first()
+            if user:
+                user_id = user.id
+            else:
+                return "Bad Request 400: You did not supply a valid API key.", 400
         else:
-            return "Bad Request 400: You did not supply a valid API key.", 400
+            return "Bad Request 400: You did not supply an API key.", 400
+    if MULTIUSER:
+        submissions = Submission.query.filter_by(user_id=user_id).order_by(Submission.id.desc()).all()
     else:
-        return "Bad Request 400: You did not supply an API key.", 400
+        submissions = Submission.query.order_by(Submission.id.desc()).all()
+    return_data = []
+    for s in submissions:
+        return_data.append({'id': s.id,
+                            'sha256': s.sha256, 'time': str(s.time),
+                            'classification': s.classification,
+                            'possible_classification': s.possible_classification,
+                            'ip_address': s.ip_address,
+                            'status': s.status})
+    return json.dumps(return_data), 200
 
 
 @app.route("/classification/<sha256>")
@@ -220,24 +232,31 @@ def classification(sha256):
     Provides the classifications for a specific hash.
     """
     ip_addr = get_request_ip()
-    if not MULTIUSER or (MULTIUSER and get_apikey()):
-        api_key = get_apikey()
-        user = User.query.filter_by(api_key=api_key).first()
-        if user:
-            submissions = Submission.query.filter_by(sha256=sha256.upper()).order_by(Submission.id.desc()).all()
-            return_data = []
-            for s in submissions:
-                return_data.append({'id': s.id,
-                                    'sha256': s.sha256, 'time': str(s.time),
-                                    'classification': s.classification,
-                                    'possible_classification': s.possible_classification,
-                                    'ip_address': s.ip_address,
-                                    'status': s.status})
-            return json.dumps(return_data), 200
+
+    user_id = None
+    if MULTIUSER:
+        if get_apikey():
+            api_key = get_apikey()
+            user = User.query.filter_by(api_key=api_key).first()
+            if user:
+                user_id = user.id
+            else:
+                return "Bad Request 400: You did not supply a valid API key.", 400
         else:
-            return "Bad Request 400: You did not supply a valid API key.", 400
+            return "Bad Request 400: You did not supply an API key.", 400
+    if MULTIUSER:
+        submissions = Submission.query.filter_by(sha256=sha256.upper()).filter_by(user_id=user_id).order_by(Submission.id.desc()).all()
     else:
-        return "Bad Request 400: You did not supply an API key.", 400
+        submissions = Submission.query.filter_by(sha256=sha256.upper()).order_by(Submission.id.desc()).all()
+    return_data = []
+    for s in submissions:
+        return_data.append({'id': s.id,
+                            'sha256': s.sha256, 'time': str(s.time),
+                            'classification': s.classification,
+                            'possible_classification': s.possible_classification,
+                            'ip_address': s.ip_address,
+                            'status': s.status})
+    return json.dumps(return_data), 200
 
 
 if __name__ == '__main__':
